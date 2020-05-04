2eefd75e94e82c79a5b4fd0209e634cc1a51344e
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-04 21:36:28 +0300

    added section reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,13 @@
 $(function(){
 
+    $('.reviews__slider-inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../images/icons/multimedia-option.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../images/icons/arrowsoutline.svg" alt=""></button>',
+      });
 
-
-    
 });
 
 
