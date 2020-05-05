fe7f0249c6a60d577c7529b3d349583c6b1a7f77
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-05 12:55:02 +0300

    added section partners

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,14 @@ $(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../images/icons/arrowsoutline.svg" alt=""></button>',
       });
 
+    $('.partners__slider-inner').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../images/icons/multimedia-option.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../images/icons/arrowsoutline.svg" alt=""></button>',
+    });
+
 });
 
 
