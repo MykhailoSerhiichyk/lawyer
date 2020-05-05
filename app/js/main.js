7088901e1e9c2b53ed054247ba74661363400346
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-05 14:16:45 +0300

    added section rate

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,15 @@ $(function(){
       nextArrow: '<button type="button" class="slick-next"><img src="../images/icons/arrowsoutline.svg" alt=""></button>',
     });
 
+    $('.rate-content__tabs .tab, .settings__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.rate-content__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.rate-content__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
 });
 
 
