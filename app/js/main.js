e2b7072c13ed2ec010e8fcc8dd1295372629e71e
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-06 12:49:28 +0300

    added amendment

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,6 @@ $(function(){
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../images/icons/multimedia-option.svg" alt=""></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../images/icons/arrowsoutline.svg" alt=""></button>',
       autoplay: true,
       autoplaySpeed: 3000,
       responsive: [
@@ -39,8 +37,6 @@ $(function(){
       infinite: true,
       slidesToShow: 4,
       slidesToScroll: 1,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../images/icons/multimedia-option.svg" alt=""></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../images/icons/arrowsoutline.svg" alt=""></button>',
       responsive: [
         {
           breakpoint: 1141,
